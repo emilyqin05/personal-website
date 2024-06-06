bdc6c252831410d422f10e09331de64c08c2fa30
emilyqin05 <123emilyqin@gmail.com>
2024-06-06 00:32:52 -0700

    Made it zoom in more

--- frontpage.js
+++ frontpage.js
@@ -44,7 +44,7 @@ const miniScreen = document.querySelector(".miniScreen");
 
 computerOverlay.addEventListener("click", function() {
     // Zoom into the computerOverlay
-    zoomElement.style.transform = `scale(4.5)`;
+    zoomElement.style.transform = `scale(4.8)`;
     zoomElement.style.transformOrigin = "59% 19%"; // Adjust to zoom into the computer
 
     // Make miniScreen interactive and scrollable
